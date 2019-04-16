4844d6f61cd600ef4a11f79e98918c8c551594d3
Stella McEwen <mcewen.stella@gmail.com>
2019-04-16 21:02:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,12 @@ def key_for_min_value(name_hash)
     maybe_value = ""
  name_hash.collect do |key, value|
    if (maybe_value == "")
-     1_key = key
-     1_value = value
+     maybe_key = key
+     maybe_value = value
   elsif (value < 1_value)   
-    1_key = key
-    1_key = value
+    maybe_key = key
+    maybe_key = value
   end
 end
-  1_key
+  maybe_key
 end
