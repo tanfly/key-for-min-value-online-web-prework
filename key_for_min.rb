9a57f4e2a2f9113be599bc3355ddc58d37cee5f4
Stella McEwen <mcewen.stella@gmail.com>
2019-04-16 21:03:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash == {}
+    return nil
     maybe_key = ""
     maybe_value = ""
  name_hash.collect do |key, value|
